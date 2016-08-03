f093c028068820728ec13293f5370a26e46ccc05
Elvis07 <798423528@qq.com>
2016-08-03 23:37:20 +0800

    add 截图 知道截图是网页有多长就有多长

--- src/com/elvis/util/PhantomjsDemo.java
+++ src/com/elvis/util/PhantomjsDemo.java
@@ -1,10 +1,19 @@
 package com.elvis.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -27,32 +36,35 @@ public class PhantomjsDemo {
 
 		for (int i = 0; i < 10; i++) {
 			driver.manage().window().maximize();
-			Thread.sleep(getRandom());
+			// Thread.sleep(getRandom());
 			driver.get(url);
-
 			wait.until(ExpectedConditions.presenceOfElementLocated(By.id("startpagebookmark")));
-			System.out.println("--------首页响应完毕--------");
 
+			System.out.println("--------首页响应完毕--------");
 			WebElement trEle = driver.findElement(By.className("hal-table-body"))
 					.findElements(By.className("hal-table-row")).get(i).findElements(By.tagName("td")).get(0)
 					.findElement(By.tagName("input"));
-
 			scrollScreen(trEle, action, jse);
-			Thread.sleep(getRandom());
+			// Thread.sleep(getRandom());
 			WebElement detailIcon = driver.findElement(By.xpath("//input[@id='tracing_by_booking_f:hl27:hl53']")); // Details按钮
 			System.out.println("details按钮");
+
 			action.moveToElement(detailIcon);
 			action.click().perform();
+			snapshot((TakesScreenshot) driver);
 			// jse.executeScript("scroll(0,450)");
+			Document doc = Jsoup.parse(driver.getPageSource());
+
 			wait.until(
 					ExpectedConditions.presenceOfElementLocated(By.xpath("//label[@id='tracing_by_booking_f:hl13']")));
-
-			System.out.println("url：" + driver.getCurrentUrl());
+			snapshot((TakesScreenshot) driver);
+			System.out.println("url: " + (i + 1) + driver.getCurrentUrl());
 
 			array1.add(i, driver.getCurrentUrl());
 
 			// parseResult(driver);
 		}
+
 		driver.quit();
 
 		for (int i = 0; i < array1.size(); i++) {
@@ -90,12 +102,38 @@ public class PhantomjsDemo {
 		}
 	}
 
-	private static int getRandom() {
+	private static int getRandom1() {
 		int Max = 10 * 1000, Min = 5 * 1000;
 
 		int a = (int) Math.round(Math.random() * (Max - Min) + Min);
 
 		return a;
+	}
+
+	public static void snapshot(TakesScreenshot drivername) {
+		// this method will take screen shot ,require two parameters ,one is
+		// driver name, another is file name
+		Date date = new Date();
+		DateFormat df3 = DateFormat.getTimeInstance();// 只显示出时分秒
+		String filename = df3.format(date) + ".png";
+		// String currentPath = System.getProperty("user.dir"); // get current
+		// work
+		// folder
+		// System.out.println(currentPath);
+		File scrFile = drivername.getScreenshotAs(OutputType.FILE);
+		// Now you can do whatever you need to do with it, for example copy
+		// somewhere
+		try {
+			System.out.println("save snapshot name is:" + filename);
+			FileUtils.copyFile(scrFile, new File("E:\\snap" + "\\" + filename));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Can't save screenshot");
+			e.printStackTrace();
+		} finally {
+
+			System.out.println("screen shot finished");
+		}
 	}
 
 }
